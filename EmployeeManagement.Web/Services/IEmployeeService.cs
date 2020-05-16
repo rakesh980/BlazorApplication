2d5911134f3774b6aadb262ee3706400ae618563
rakesh980 <rakesh980980@gmail.com>
2020-05-16 23:59:43 +0530

    Commited all files

---
+++ EmployeeManagement.Web/Services/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagementModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
+        Task<Employee> UpdateEmployee(Employee updatedEmployee);
+        Task<Employee> CreateEmployee(Employee newEmployee);
+    }
+}
